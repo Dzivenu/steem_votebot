155011751b70880a83faf62a6d275c27b6223e07
Riverhead <james.reidy@gmail.com>
2016-09-28 08:36:59 -0400

    Running bot, just add accounts

--- votebot.py
+++ votebot.py
@@ -1,9 +1,10 @@
 from steemapi.steemclient import SteemClient
 import datetime
+from datetime import timedelta,tzinfo
 import time
 import os
 import json
-
+import sys
 
 class Config():
  # Port and host of the RPC-HTTP-Endpoint of the wallet
@@ -19,32 +20,44 @@ class Config():
 
 client = SteemClient(Config)
 
-# Account to vote as
-whale = "riverhead" # Account to vote as
+# Accounts to vote with
+whales = [""] 
 
-#Projects (Accounts, users, etc.) to follow
-follow = ["curie","positivity"]
-tags = ["project-positivity"]
+#Accounts to vote as
+follow = ["curie","positivity","littlekitty"]
+
+#tags = ["project-positivity"]
 
 #Get posts of those we follow:
-
 startFrom = -1
-limit = 500
+limit = 100
 
-whale_trans = client.rpc.get_account_history(whale,startFrom,limit)
-
-for account in follow:
-  transactions = client.rpc.get_account_history(account,startFrom,limit)
-  for transaction in transactions:
-    if transaction[1]['op'][0] == "vote" and transaction[1]['op'][1]['voter'] == account:
-      voted = 0
-
-      for wtrans in whale_trans:
-        if wtrans[1]['op'][0] == "vote" and wtrans[1]['op'][1]['voter'] == whale:
-          if wtrans[1]['op'][1]['permlink'] == transaction[1]['op'][1]['permlink']:
-            voted = 1;
-
-      if voted == 0:
-        print("Voting on: %s" % (transaction[1]['op'][1]['permlink']))
-        client.rpc.vote(whale, transaction[1]['op'][1]['author'], transaction[1]['op'][1]['permlink'],100,'true')
-
+#Vote on posts our tracked accounts vote on that are older than 15 minutes but newer than 24 hours.
+while True:
+  for account in follow:
+    transactions = client.rpc.get_account_history(account,startFrom,limit)
+    for transaction in transactions:
+      if transaction[1]['op'][0] == "vote" and transaction[1]['op'][1]['voter'] == account:
+  
+        vt = transaction[1]['timestamp']
+        ht = client.rpc.info()['time'] 
+  
+        vT = datetime.datetime(int(float(vt[0:-15])), int(float(vt[5:-12])), int(float(vt[8:10])), int(float(vt[11:-6])), int(float(vt[14:-3])), int(float(vt[17:])))
+        hT = datetime.datetime(int(float(ht[0:-15])), int(float(ht[5:-12])), int(float(ht[8:10])), int(float(ht[11:-6])), int(float(ht[14:-3])), int(float(ht[17:])))
+  
+        for whale in whales:
+          whale_trans = client.rpc.get_account_history(whale,startFrom,limit)
+          voted = 0
+  
+          for wtrans in whale_trans:
+            if wtrans[1]['op'][0] == "vote" and wtrans[1]['op'][1]['voter'] == whale:
+              if wtrans[1]['op'][1]['permlink'] == transaction[1]['op'][1]['permlink']:
+                voted = 1;
+  
+          if voted == 0:
+            age = (hT - vT) / timedelta(hours=1)
+            if age < 24 and age > 0.25:
+              print("%0.2f %s %s" % (age, whale, transaction[1]['op'][1]['permlink']))
+              client.rpc.vote(whale, transaction[1]['op'][1]['author'], transaction[1]['op'][1]['permlink'],100,'true')
+  
+  
